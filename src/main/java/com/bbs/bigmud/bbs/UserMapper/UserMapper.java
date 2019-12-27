98d3be5ff2dc24f436affcdc2f3de925ccf3866a
bigmud <bigmudhaijian@gmail.com>
2019-12-27 17:09:03 +0800

    add check token

--- src/main/java/com/bbs/bigmud/bbs/UserMapper/UserMapper.java
+++ src/main/java/com/bbs/bigmud/bbs/UserMapper/UserMapper.java
@@ -5,6 +5,7 @@ import com.bbs.bigmud.bbs.Model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
@@ -13,5 +14,6 @@ public interface UserMapper {
     @Insert("insert into USER(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{account_id},#{token},#{gmt_Create},#{gmt_Modified})")
     void insert(User user);
 
-
+    @Select("select * from user where token = #{token}")
+    User findByToken(String token);
 }
