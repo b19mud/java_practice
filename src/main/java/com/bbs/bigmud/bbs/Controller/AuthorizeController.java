1e645cb636c240bc44d2a4b2ff043ce0d0544a42
bigmud <bigmudhaijian@gmail.com>
2019-12-27 12:20:46 +0800

    session

--- src/main/java/com/bbs/bigmud/bbs/Controller/AuthorizeController.java
+++ src/main/java/com/bbs/bigmud/bbs/Controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
   *
  */
@@ -27,7 +29,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(secretId);
@@ -37,6 +40,21 @@ public class AuthorizeController {
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
         System.out.println(user.getName());
-        return "index";
+
+        //session ,cookie验证
+        if(user!=null)
+        {
+            //sucess
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }
+        else{
+            //fail
+            return "redirect:/";
+        }
+
+        //token 验证
+
+
     }
 }
