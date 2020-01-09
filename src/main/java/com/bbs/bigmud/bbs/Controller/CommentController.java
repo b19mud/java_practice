3e55b692860b691454351e79e999a56ab11fdf6f
bigmud <bigmudhaijian@gmail.com>
2020-01-09 15:55:19 +0800

    comment step1

---
+++ src/main/java/com/bbs/bigmud/bbs/Controller/CommentController.java
@@ -0,0 +1,32 @@
+package com.bbs.bigmud.bbs.Controller;
+
+
+import com.bbs.bigmud.bbs.Mapper.CommentMapper;
+import com.bbs.bigmud.bbs.Model.Comment;
+import com.bbs.bigmud.bbs.dto.CommentDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class CommentController {
+
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @ResponseBody
+    @RequestMapping(value = "/comment",method = RequestMethod.POST)
+    public Object post(@RequestBody CommentDTO commentDTO){
+
+        Comment comment = new Comment();
+        comment.setParentId(commentDTO.getParentId());
+        comment.setContent(commentDTO.getContent());
+        comment.setType(commentDTO.getType());
+        comment.setGmtCreate(System.currentTimeMillis());
+        comment.setGmtModified(System.currentTimeMillis());
+        comment.setCommentator(1);
+        commentMapper.insert(comment);
+
+        return null;
+    }
+}
