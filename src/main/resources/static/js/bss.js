5890272f54d314d1eb5a00a5da9667348b0ab292
bigmud <bigmudhaijian@gmail.com>
2020-01-15 21:43:17 +0800

    front problem

--- src/main/resources/static/js/bss.js
+++ src/main/resources/static/js/bss.js
@@ -14,7 +14,16 @@ function post(){
             if (response.code == 200){
                 $("#comment_section").hide()
             }else{
-                alert(response.message())
+                if(response.code == 2003){
+                    var isAccept = confirm(response.message);
+                    if(isAccept){
+                        window.open("https://github.com/login/oauth/authorize?client_id=8897d5437c00e01f2b5d&redirect_uri=http://localhost:8887/callback&scope=user&state=1")
+                        window.localStorage.setItem("closable",true);
+                    }
+                }else{
+                    alert(response.message)
+                }
+
             }
                 console.log("sssss");
         }
